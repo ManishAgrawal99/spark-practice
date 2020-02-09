f68090543e603ec4471f4dd512a93e88d1ead7b3
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2020-02-09 17:42:47 +0530

    Making use of ReduceByKey on PairedRDD to get single results on each unique key

--- src/main/java/com/virtualpairprogrammers/Main.java
+++ src/main/java/com/virtualpairprogrammers/Main.java
@@ -19,9 +19,9 @@ public class Main {
 		//The data we will import in SPARK RDDs
 		List<String> inputData = new ArrayList<String>();
 		inputData.add("WARN: Tuesday 4 September 0405");
-		inputData.add("WARN: Tuesday 4 September 0408");
-		inputData.add("WARN: Wednesday 5 September 1632");
-		inputData.add("WARN: Thursday 7 September 1854");
+		inputData.add("ERROR: Tuesday 4 September 0408");
+		inputData.add("FATAL: Wednesday 5 September 1632");
+		inputData.add("ERROR: Thursday 7 September 1854");
 		inputData.add("WARN: Friday 8 September 1942");
 		
 		//Setting up Spark
@@ -33,12 +33,17 @@ public class Main {
 		//Importing the data into RDDs from List
 		JavaRDD<String> originalLogMessages = sc.parallelize(inputData);
 		
-		JavaPairRDD<String, String> pairRdd = originalLogMessages.mapToPair(rawValue -> {
+		JavaPairRDD<String, Long> pairRdd = originalLogMessages.mapToPair(rawValue -> {
 			String[] pairs = rawValue.split(":");
 			String levels = pairs[0];
-			String date = pairs[1];
-			return new Tuple2<String, String>(levels, date);
+			return new Tuple2<String, Long>(levels, 1L);
 		});
+		//Printing to look how the data is arranged
+		pairRdd.foreach(value -> System.out.println(value));
+		
+		JavaPairRDD<String, Long> countRdd = pairRdd.reduceByKey((value1, value2)->value1 + value2);
+		countRdd.foreach(tuple -> System.out.println(tuple._1 + ": "+tuple._2));
+		
 		
 		sc.close();
 	}
